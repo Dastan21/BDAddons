ee6b898d20e2f23b8de13cb9e15f161c93c918f9
Dastan21 <loicd21@gmail.com>
2021-04-22 23:30:12 +0200

    Fixed bad selector for adding star on medias

--- plugins/FavoriteMedia/FavoriteMedia.plugin.js
+++ plugins/FavoriteMedia/FavoriteMedia.plugin.js
@@ -12,7 +12,7 @@ class FavoriteMedia {
 		info: {
 			name: "FavoriteMedia",
 			author: "Dastan21",
-			version: "1.4.7",
+			version: "1.4.8",
 			description: "Adds media tabs, on the GIF/Emojis panel, to post favorited medias such as images, videos and audios."
 		}
 	};
@@ -87,6 +87,7 @@ class FavoriteMedia {
 		searchMedium: this.modules.inn.medium,
 		searchInner: this.modules.inn.inner,
 		searchInput: this.modules.inn.input,
+		embedFull: BdApi.findModuleByProps("spoilerBlurRadius", "embed", "embedFull").embedFull,
 	};
 	favsvg_filled = `<svg class="${this.classes.size}" aria-hidden="false" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12.5,17.6l3.6,2.2a1,1,0,0,0,1.5-1.1l-1-4.1a1,1,0,0,1,.3-1l3.2-2.8A1,1,0,0,0,19.5,9l-4.2-.4a.87.87,0,0,1-.8-.6L12.9,4.1a1.05,1.05,0,0,0-1.9,0l-1.6,4a1,1,0,0,1-.8.6L4.4,9a1.06,1.06,0,0,0-.6,1.8L7,13.6a.91.91,0,0,1,.3,1l-1,4.1a1,1,0,0,0,1.5,1.1l3.6-2.2A1.08,1.08,0,0,1,12.5,17.6Z"/></svg>`;
 	favsvg_notfilled = `<svg class="${this.classes.iconGif}" aria-hidden="false" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M19.6,9l-4.2-0.4c-0.4,0-0.7-0.3-0.8-0.6l-1.6-3.9c-0.3-0.8-1.5-0.8-1.8,0L9.4,8.1C9.3,8.4,9,8.6,8.6,8.7L4.4,9 c-0.9,0.1-1.2,1.2-0.6,1.8L7,13.6c0.3,0.2,0.4,0.6,0.3,1l-1,4.1c-0.2,0.9,0.7,1.5,1.5,1.1l3.6-2.2c0.3-0.2,0.7-0.2,1,0l3.6,2.2 c0.8,0.5,1.7-0.2,1.5-1.1l-1-4.1c-0.1-0.4,0-0.7,0.3-1l3.2-2.8C20.9,10.2,20.5,9.1,19.6,9z M12,15.4l-3.8,2.3l1-4.3l-3.3-2.9 l4.4-0.4l1.7-4l1.7,4l4.4,0.4l-3.3,2.9l1,4.3L12,15.4z"/></svg>`;
@@ -657,7 +658,8 @@ class FavoriteMedia {
 	}
 	checkForMedias(node) {
 		for (let media of node.childNodes) {
-			const img = media.querySelector("img"); if (img) this.addFavButtonOnImage(img.parentNode);
+			if (media.classList.contains(this.classes.embedFull.split(' ')[0])) return;
+			const img = media.querySelector("a > img"); if (img) this.addFavButtonOnImage(img.parentNode);
 			const vid = media.querySelector("video"); if (vid) this.addFavButtonOnVideo(vid);
 			const aud = media.querySelector("audio"); if (aud) this.addFavButtonOnAudio(aud.parentNode.firstChild);
 		}
