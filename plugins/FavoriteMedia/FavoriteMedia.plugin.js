84df261953da087093e519cacea7d3aa0539ab3c
Dastan21 <loicd21@gmail.com>
2021-07-12 09:40:17 +0200

    [FavoriteMedia] Fixed bug - Buttons are not showing again in DM channel

--- plugins/FavoriteMedia/FavoriteMedia.plugin.js
+++ plugins/FavoriteMedia/FavoriteMedia.plugin.js
@@ -4,7 +4,7 @@
  * @author Dastan
  * @authorId 310450863845933057
  * @authorLink https://github.com/Dastan21
- * @version 0.2.3
+ * @version 0.2.4
  * @source https://github.com/Dastan21/BDAddons/blob/main/plugins/FavoriteMedia
  */
 
@@ -14,7 +14,7 @@ const FavoriteMedia = (() => {
 			name: "FavoriteMedia",
 			authors: [{ name: "Dastan", github_username: "Dastan21", discord_id: "310450863845933057" }],
 			description: "Allows to favorite images, videos and audios. Adds tabs to the emojis menu to see your favorited medias.",
-			version: "0.2.3",
+			version: "0.2.4",
 			github: "https://github.com/Dastan21/BDAddons/tree/main/plugins/FavoriteMedia",
 			github_raw: "https://github.com/Dastan21/BDAddons/blob/main/plugins/FavoriteMedia/FavoriteMedia.plugin.js"
 		},
@@ -125,7 +125,7 @@ const FavoriteMedia = (() => {
 				title: "Fixed",
 				type: "fixed",
 				items: [
-					"Buttons will no longer be displayed in uploading file modal"
+					"Buttons are not showing again in DM channel"
 				]
 			}
 		]
@@ -1638,7 +1638,8 @@ const FavoriteMedia = (() => {
 				patchChannelTextArea() {
 					Patcher.after(ChannelTextArea, "render", (_, [props], returnValue) => {
 						if (props.className.includes("Upload")) return;
-						if (!Permissions.can(PermissionsConstants.SEND_MESSAGES, UserStore.getCurrentUser().id, ChannelStore.getChannel(SelectedChannelStore.getChannelId()))) return;
+						const channel = ChannelStore.getChannel(SelectedChannelStore.getChannelId());
+						if (!channel.type && !Permissions.can(PermissionsConstants.SEND_MESSAGES, UserStore.getCurrentUser().id, channel)) return;
 						const buttons = Utilities.findInReactTree(returnValue, e => e && e.className && e.className.startsWith("buttons"));
 						if (!buttons || !Array.isArray(buttons.children)) return;
 						if (this.settings.btnsPosition === "left") {
