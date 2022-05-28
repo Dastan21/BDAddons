1a2fb86b567a799d0eb9cdb6bc9761c5ecd4c90f
Dastan21 <loicd21@gmail.com>
2022-05-28 17:14:51 +0200

    [FavoriteMedia] Fixed image/video preview

--- plugins/FavoriteMedia/FavoriteMedia.plugin.js
+++ plugins/FavoriteMedia/FavoriteMedia.plugin.js
@@ -4,7 +4,7 @@
  * @author Dastan
  * @authorId 310450863845933057
  * @authorLink https://github.com/Dastan21
- * @version 1.6.0
+ * @version 1.6.1
  * @source https://github.com/Dastan21/BDAddons/blob/main/plugins/FavoriteMedia
  */
 
@@ -14,7 +14,7 @@ module.exports = (() => {
 			name: "FavoriteMedia",
 			authors: [{ name: "Dastan", github_username: "Dastan21", discord_id: "310450863845933057" }],
 			description: "Allows to favorite images, videos and audios. Adds tabs to the emojis menu to see your favorited medias.",
-			version: "1.6.0",
+			version: "1.6.1",
 			github: "https://github.com/Dastan21/BDAddons/tree/main/plugins/FavoriteMedia",
 			github_raw: "https://github.com/Dastan21/BDAddons/blob/main/plugins/FavoriteMedia/FavoriteMedia.plugin.js"
 		},
@@ -960,7 +960,7 @@ module.exports = (() => {
 						this.state.visible ? React.createElement(this.props.type === "audio" ? "audio" : this.state.showControls || this.props.type === "gif" ? "video" : "img", {
 							className: classes.result.gif,
 							preload: "auto",
-							src: this.props.type === "video" && !this.state.showControls ? this.props.poster : this.props.src,
+							src: this.props.type === "video" && !this.state.showControls ? this.props.poster : (this.props.src || this.props.url),
 							poster: this.props.poster,
 							width: this.props.positions.width,
 							height: this.props.positions.height,
