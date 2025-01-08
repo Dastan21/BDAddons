63acc1e0d35d9ee865e17b92583ca73924633d72
Dastan21 <loicd21@gmail.com>
2025-01-08 13:40:37 +0100

    chores: update deps & add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  printWidth: 120,
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  useTabs: false,
+}
+
+export default config
