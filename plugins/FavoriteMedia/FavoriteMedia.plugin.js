ba958e70079a59dfefdf94b2d958c547e060140c
Bob Carter <46971019+bob-carter@users.noreply.github.com>
2026-08-17 16:45:23 +0000

    Fixed paging for Fav media
    Just removed dependency and tries to fix some errors for things I broke.

--- plugins/FavoriteMedia/FavoriteMedia.plugin.js
+++ plugins/FavoriteMedia/FavoriteMedia.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name FavoriteMedia
  * @description Allows to favorite GIFs, images, videos, audios and files.
- * @version 1.13.33
+ * @version 1.13.33E
  * @author Dastan
  * @authorId 310450863845933057
  * @source https://github.com/Dastan21/BDAddons/blob/main/plugins/FavoriteMedia
